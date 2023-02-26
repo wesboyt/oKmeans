e3791ef82e3dbb38acb62839b3dcadcac2d4719d
wesboyt <tie.yob@gmail.com>
2023-02-26 08:27:20 -0800

    fixes indexing bugs.

--- cluster.h
+++ cluster.h
@@ -1,47 +1,53 @@
 #include <torch/torch.h>
 #include "wasserstein.h"
-//#include "combination.h"
 #include <algorithm>
 #include <random>
 
 std::vector<int> cluster(std::vector<float> input, int k) {
-    auto base = torch::tensor(input.data());
+    k = k - 1;
+    auto base = torch::zeros(input.size());
+    for(int i = 0; i < input.size(); ++i) {
+        base[i] = input[i];
+    }
+
     std::vector<int> ic = std::vector<int>(input.size());
     for(int i = 0; i < input.size(); i++){
         ic[i] = i;
     }
-    //auto kCombos = combination(ic, k - 1);
+
     auto rd = std::random_device {};
     auto rng = std::default_random_engine { rd() };
 
     float minD = std::numeric_limits<float>::max();
     std::vector<int> best;
-    torch::Tensor adjusted = torch::zeros({1, (int)input.size()});
+    torch::Tensor adjusted = torch::zeros({(int)input.size()});
 
     for(int l = 0; l < 10000; ++l) {
         std::shuffle(ic.begin(), ic.end(), rng);
-        auto indexes = std::vector<int>(ic.begin(), ic.begin() + (k - 1));
+        auto indexes = std::vector<int>(ic.begin(), ic.begin() + (k));
         std::sort(indexes.begin(), indexes.end());
 
-        float adjWeight = std::reduce(input.begin(), input.begin() + indexes[0]) / indexes[0];
+        float adjWeight = std::accumulate(input.begin(), input.begin() + indexes[0],0.0) / (indexes[0] + 1);
         for(int j = 0; j < indexes[0]; ++j) {
-            adjusted[j = adjWeight];
+            adjusted[j] = adjWeight;
         }
 
         for(int i = 1; i < k - 1; ++i) {
-            adjWeight = std::reduce(input.begin() + indexes[i-1], input.begin() + indexes[i]) / (indexes[i] - indexes[i-1]);
+            adjWeight = std::accumulate(input.begin() + indexes[i-1], input.begin() + indexes[i], 0.0) / ((indexes[i] - indexes[i-1]) + 1);
+
             for(int j = indexes[i-1]; j < indexes[i]; ++j) {
-                adjusted[j = adjWeight];
+                adjusted[j] = adjWeight;
             }
         }
 
-        adjWeight = std::reduce(input.begin() + indexes[k - 1], input.end()) / (indexes[k - 2] - indexes[k - 1]);
-        for(int j = indexes[k - 2]; j < indexes[k - 1]; ++j) {
-            adjusted[j = adjWeight];
+        adjWeight = std::accumulate(input.begin() + indexes[k - 1], input.end(), 0.0) / ((indexes[k - 1] - indexes[k - 2]) + 1);
+
+        for(int j = indexes[k - 1]; j < input.size(); ++j) {
+            adjusted[j] = adjWeight;
         }
 
         float d = distance(base, adjusted, 1).item<float>();
-        if(d < minD){
+        if(d < minD) {
             minD = d;
             best = indexes;
             std::cout << "Distance: " << d  << ", Indexes: "<< indexes << std::endl;
