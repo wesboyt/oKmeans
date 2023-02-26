05a4abd200bde29ec2494ff38c0cd1324a8ba75a
wesboyt <tie.yob@gmail.com>
2023-02-26 13:37:17 -0800

    100x faster.

--- cluster.h
+++ cluster.h
@@ -20,41 +20,34 @@ std::vector<int> cluster(std::vector<float> input, int k) {
 
     float minD = std::numeric_limits<float>::max();
     std::vector<int> best;
-    auto adjusted = torch::zeros(input.size());
-
-    for(int l = 0; l < 1000000; ++l) {
+    torch::Tensor compare = torch::zeros((int)input.size());
+    for(int l = 0; l < 10000000; ++l) {
         std::shuffle(ic.begin(), ic.end(), rng);
         auto indexes = std::vector<int>(ic.begin(), ic.begin() + k);
         std::sort(indexes.begin(), indexes.end());
         int length = indexes[0] + 1;
         int start = 0;
         float adjWeight = std::reduce(input.begin() + start, input.begin() + length) / length;
-        for(int j = 0; j <= indexes[0]; ++j) {
-            adjusted[j] = adjWeight;
-        }
+        std::vector<float> adjusted = std::vector<float>();
+        adjusted.insert(adjusted.end(),length, adjWeight);
+
 
         for(int i = 1; i < k; ++i) {
             start = indexes[i-1] + 1;
             length = indexes[i] - indexes[i-1];
             adjWeight = std::reduce(input.begin() + start, input.begin() + start + length) / (length);
+            adjusted.insert(adjusted.end(),length, adjWeight);
 
-            for(int j = indexes[i-1] + 1; j <= indexes[i]; ++j) {
-                adjusted[j] = adjWeight;
-            }
         }
         start = indexes[k - 1] + 1;
         adjWeight = std::reduce(input.begin() + start, input.end()) / (input.size() - start);
-
-        for(int j = indexes[k - 1] + 1; j < input.size(); ++j) {
-            adjusted[j] = adjWeight;
-        }
-
-        float d = distance(base, adjusted, 1).item<float>();
+        length = input.size() - start;
+        adjusted.insert(adjusted.end(),length, adjWeight);
+        float d = distance(base, torch::from_blob(adjusted.data(), {4823},{torch::kFloat32}), 1).item<float>();
         if(d < minD) {
             minD = d;
             best = indexes;
             std::cout << "Distance: " << d  << ", Indexes: "<< indexes << std::endl;
-            //std::cout << adjusted << std::endl;
         }
     }
     return best;
